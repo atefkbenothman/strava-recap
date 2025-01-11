dce9c7ab0230d10dd9d340ac37f90038b543fb7c
atefkbenothman <atefkbenothman@gmail.com>
2025-01-11 00:30:47 -0800

    update yearpicker css

--- src/components/nav/yearPicker.tsx
+++ src/components/nav/yearPicker.tsx
@@ -16,19 +16,19 @@ export default function YearPicker() {
       <PaginationContent>
         <PaginationItem>
           <PaginationPrevious
-            className="hover:bg-transparent p-0 hover:cursor-pointer hover:underline dark:hover:text-white"
+            className="flex items-center justify-center text-xs hover:bg-transparent p-0 hover:cursor-pointer hover:underline dark:hover:text-white"
             title={String((currentYear ?? 0) - 1)}
             onClick={(() => updateYear(currentYear - 1))}
           />
         </PaginationItem>
         <PaginationItem>
-          <PaginationLink className="font-bold hover:bg-transparent mx-4 dark:hover:text-white">
+          <PaginationLink className="flex text-center text-md justify-center font-bold hover:bg-transparent mx-4 dark:hover:text-white">
             {currentYear}
           </PaginationLink>
         </PaginationItem>
         <PaginationItem>
           <PaginationNext
-            className="hover:bg-transparent p-0 hover:cursor-pointer hover:underline dark:hover:text-white"
+            className="flex items-center justify-center text-xs hover:bg-transparent p-0 hover:cursor-pointer hover:underline dark:hover:text-white"
             title={String((currentYear ?? 0) + 1)}
             onClick={() => updateYear(currentYear + 1)}
           />

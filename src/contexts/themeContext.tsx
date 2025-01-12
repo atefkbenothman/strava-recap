97342db12981ef7e23d3709908a8971d53976b05
atefkbenothman <atefkbenothman@gmail.com>
2025-01-12 04:03:38 -0800

    add more colors

--- src/contexts/themeContext.tsx
+++ src/contexts/themeContext.tsx
@@ -7,10 +7,10 @@ export type ColorPalette = {
 
 export const Themes = {
   Default: ["#06d6a0", "#20aaa1", "#198190", "#0e5d72", "#073b4c", "#456C78", "#739099", "#96ABB2", "#B1C0C5", "#C4D0D3", "#D6DFE1", "#E0E7E8", "#E8EDEE"],
-  Sunset: ["#0055FFFF", "#3399FFFF", "#66CCFFFF", "#99EEFFFF", "#CCFFFFFF", "#FFFFCCFF", "#FFEE99FF", "#FFCC66FF", "#FF9933FF", "#FF5500FF"],
-  Red: ["#9e0142", "#d53e4f", "#f46d43", "#fdae61", "#fee08b", "#ffffbf", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#5e4fa2"],
-  Ocean: ["#235D72FF", "#3A7C89FF", "#559C9EFF", "#7BBCB0FF", "#A5DBC2FF", "#D2FBD4FF"],
-  Orange: ["#990000", "#d7301f", "#ef6548", "#fc8d59", "#fdbb84", "#fdd49e"],
+  Sunset: ["#0055ff", "#3399ff", "#66ccff", "#99eeff", "#ccffff", "#ffffcc", "#ffee99", "#ffcc66", "#ff9933", "#ff5500", "#ff7f3f", "#ffa06e", "#ffb892"],
+  Red: ["#9e0142", "#d53e4f", "#f46d43", "#fdae61", "#fee08b", "#ffffbf", "#e6f598", "#abdda4", "#66c2a5", "#3288bd", "#486cb0", "#5e4fa2", "#867bb9"],
+  Ocean: ["#18404e", "#1d4d5e", "#235d72", "#327181", "#3a7C89", "#559C9e", "#7BBcb0", "#9bd4be", "#a5dbc2", "#d2fbd4", "#e0fbe2", "#eafbeb", "#f1fbf1"],
+  Orange: ["#690000", "#990000", "#b81810", "#d7301f", "#ef6548", "#bd6943", "#dd7b4e", "#fc8d59", "#fdbb84", "#fdd49e", "#fde2bd", "#fdebd2", "#fdf1e0"],
 } as const
 
 export type Theme = keyof typeof Themes

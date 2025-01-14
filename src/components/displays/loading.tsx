06e056a312d9eac70356d3aa6acce455fc04cbc4
atefkbenothman <atefkbenothman@gmail.com>
2025-01-14 15:06:53 -0800

    animate loading screen

--- src/components/displays/loading.tsx
+++ src/components/displays/loading.tsx
@@ -12,7 +12,10 @@ export default function Loading() {
         <div className="w-full h-full flex items-center justify-center">
           <div className="flex-col p-2 space-y-4">
             <div>
-              <p>Retrieving <span className="font-bold text-xl">{currentYear}</span> activities...</p>
+              <p>
+                Retrieving <span className="font-bold text-xl">{currentYear}</span> activities
+                <span className="loading-dots inline-block w-[12px]"></span>
+              </p>
             </div>
           </div>
         </div>

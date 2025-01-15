cc79c01718d32cf5ab1845c5f6f537421c784d12
atefkbenothman <atefkbenothman@gmail.com>
2025-01-14 18:50:49 -0800

    hide empty label

--- src/components/common/stat.tsx
+++ src/components/common/stat.tsx
@@ -7,7 +7,9 @@ type StatProps = {
 export default function Stat({ label, value, unit }: StatProps) {
   return (
     <div className="flex flex-col bg-gray-200 rounded p-2 gap-4 dark:bg-[#222628]">
-      <p className="text-[10px] dark:text-white/75 text-black/75">{label}</p>
+      {label ? (
+        <p className="text-[10px] dark:text-white/75 text-black/75">{label}</p>
+      ) : null}
       <div className="flex flex-col w-full h-full items-center justify-center">
         <p className="font-semibold text-xl">
           {value}
